f189d93968b33950813e865e76614bce8b7fcfbb
Varsha Kushwah <varshakushwah711@gmail.com>
2025-03-24 23:18:49 +0530

    Create StringInterpolation.cs

---
+++ StringInterpolation.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "varsha";
+        int age = 21;
+        string message = $"Hello, {name}! You are {age} years old.";
+
+        Console.WriteLine(message);
+    }
+}
